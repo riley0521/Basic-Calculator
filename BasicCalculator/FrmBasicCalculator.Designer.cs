b37fc66c87d28b53ab0e72550eda2a309d3800ab
Riley Farro <rileyfarro@gmail.com>
2020-12-29 13:29:06 +0800

    Initial

---
+++ BasicCalculator/FrmBasicCalculator.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace BasicCalculator
+{
+    partial class FrmBasicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextNum1 = new System.Windows.Forms.TextBox();
+            this.TextNum2 = new System.Windows.Forms.TextBox();
+            this.CmbOperations = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LabelTotal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnCompute = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // TextNum1
+            // 
+            this.TextNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextNum1.Location = new System.Drawing.Point(17, 95);
+            this.TextNum1.Name = "TextNum1";
+            this.TextNum1.Size = new System.Drawing.Size(338, 35);
+            this.TextNum1.TabIndex = 1;
+            // 
+            // TextNum2
+            // 
+            this.TextNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextNum2.Location = new System.Drawing.Point(17, 179);
+            this.TextNum2.Name = "TextNum2";
+            this.TextNum2.Size = new System.Drawing.Size(338, 35);
+            this.TextNum2.TabIndex = 2;
+            // 
+            // CmbOperations
+            // 
+            this.CmbOperations.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmbOperations.FormattingEnabled = true;
+            this.CmbOperations.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.CmbOperations.Location = new System.Drawing.Point(124, 136);
+            this.CmbOperations.Name = "CmbOperations";
+            this.CmbOperations.Size = new System.Drawing.Size(121, 37);
+            this.CmbOperations.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.LabelTotal);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(17, 220);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 103);
+            this.panel1.TabIndex = 4;
+            // 
+            // LabelTotal
+            // 
+            this.LabelTotal.AutoSize = true;
+            this.LabelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTotal.ForeColor = System.Drawing.Color.Lime;
+            this.LabelTotal.Location = new System.Drawing.Point(3, 54);
+            this.LabelTotal.Name = "LabelTotal";
+            this.LabelTotal.Size = new System.Drawing.Size(0, 29);
+            this.LabelTotal.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(3, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total:";
+            // 
+            // BtnCompute
+            // 
+            this.BtnCompute.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCompute.Location = new System.Drawing.Point(111, 340);
+            this.BtnCompute.Name = "BtnCompute";
+            this.BtnCompute.Size = new System.Drawing.Size(148, 49);
+            this.BtnCompute.TabIndex = 5;
+            this.BtnCompute.Text = "Compute";
+            this.BtnCompute.UseVisualStyleBackColor = true;
+            this.BtnCompute.Click += new System.EventHandler(this.BtnCompute_Click);
+            // 
+            // FrmBasicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 401);
+            this.Controls.Add(this.BtnCompute);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.CmbOperations);
+            this.Controls.Add(this.TextNum2);
+            this.Controls.Add(this.TextNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicCalculator";
+            this.Text = "FrmBasicCalculator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextNum1;
+        private System.Windows.Forms.TextBox TextNum2;
+        private System.Windows.Forms.ComboBox CmbOperations;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LabelTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnCompute;
+    }
+}
+
---
+++ BasicCalculator/FrmBasicCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorPrivateAssembly;
+
+namespace BasicCalculator
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCompute_Click(object sender, EventArgs e)
+        {
+            if(CmbOperations.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select operation", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (TextNum1.Text.Length > 0 && float.TryParse(TextNum1.Text, out float num1) &&
+                TextNum2.Text.Length > 0 && float.TryParse(TextNum2.Text, out float num2)) {
+
+                if (CmbOperations.SelectedItem.ToString().Equals("+"))
+                {
+                    float answer = BasicComputation.Addition(num1, num2);
+
+                    LabelTotal.Text = answer.ToString();
+                }
+
+                if (CmbOperations.SelectedItem.ToString().Equals("-"))
+                {
+                    float answer = BasicComputation.Subtraction(num1, num2);
+
+                    LabelTotal.Text = answer.ToString();
+                }
+
+                if (CmbOperations.SelectedItem.ToString().Equals("*"))
+                {
+                    float answer = BasicComputation.Multiplication(num1, num2);
+
+                    LabelTotal.Text = answer.ToString();
+                }
+
+                if (CmbOperations.SelectedItem.ToString().Equals("/"))
+                {
+                    float answer = BasicComputation.Division(num1, num2);
+
+                    LabelTotal.Text = answer.ToString();
+                }
+            } else
+            {
+                MessageBox.Show("Put numbers in the 2 textbox", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
